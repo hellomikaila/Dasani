063e331513293e3e80d666c68298c9f982897598
Mikaila Weaver <me@mikailaweaver.com>
2016-03-09 16:58:55 -0500

    Cap the Tap Website
    First Commit

---
+++ app/javascripts/bgvideo-vimeo-init.js
@@ -0,0 +1,42 @@
+// BGVIDEO-INIT.JS
+//--------------------------------------------------------------------------------------------------------------------------------
+//This is  JS file that activates element animation effects used in this template*/
+// -------------------------------------------------------------------------------------------------------------------------------
+// Template Name: DIGNITY.
+// Author: Designova.
+// Website: http://www.designova.net 
+// Copyright: (C) 2013 
+// -------------------------------------------------------------------------------------------------------------------------------
+
+/*global $:false */
+/*global window: false */
+
+(function(){
+  "use strict";
+
+
+$(function ($) {
+
+	$.okvideo({ source: '125488978', //set your vimeo video source here
+		                    loop: true,
+		                    hd:true, 
+		                    autoplay: true,
+		                    volume:0 // control the volume by setting a value from 0 to 99
+
+
+	 });
+   
+});
+// $(function ($)  : ends
+
+})();
+//  JSHint wrapper $(function ($)  : ends
+
+
+
+
+
+
+
+	
+
